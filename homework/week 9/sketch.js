2ff30359fed402dbc5e94547b90a75b7ab4f3829
allingoodfaith <97848166+allingoodfaith@users.noreply.github.com>
2022-03-27 14:13:49 -0600

    more homework 9 issues

--- homework/week 9/sketch.js
+++ homework/week 9/sketch.js
@@ -5,5 +5,13 @@ function setup()
 function draw()
 {
     background(220);
+    circle(100,250,30);
+    circle(300,250,30);
+    rect(300,250,50,60);
     ellipse(250,250,100,150);
+    circle(200,250,20);
+    circle(200,250,50);
+    circle(300,250,20);
+    circle(300,250,50);
+    triangle(240,65,250,75,260,65);
 }
